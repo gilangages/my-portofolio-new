716d0e85021b46411447bcdb1f3f67e12bcf11cd
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-02-08 13:49:06 +0700

    setup api for project-api

--- frontend/src/components/llib/api/ProjectApi.js
+++ frontend/src/components/llib/api/ProjectApi.js
@@ -1,3 +1,21 @@
+export const getAllProjects = async () => {
+  return await fetch(`${import.meta.env.VITE_APP_PATH}/projects`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+};
+
+export const getSingleProject = async (id) => {
+  return await fetch(`${import.meta.env.VITE_APP_PATH}/projects${id}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+};
+
 export const adminUploadProject = async (token, formData) => {
   return await fetch(`${import.meta.env.VITE_APP_PATH}/projects`, {
     method: "POST",
@@ -6,5 +24,26 @@ export const adminUploadProject = async (token, formData) => {
       Accept: "application/json",
     },
     body: formData,
+  });
+};
+
+export const adminUpdateProject = async (token, id, formData) => {
+  return await fetch(`${import.meta.env.VITE_APP_PATH}/projects${id}`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+    },
+    body: formData,
+  });
+};
+
+export const adminDeleteProject = async (token, id) => {
+  return await fetch(`${import.meta.env.VITE_APP_PATH}/projects/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+    },
   });
 };
