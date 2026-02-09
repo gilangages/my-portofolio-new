a92b44c388ab828cd727e2e79fbbccfa439400e0
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-02-09 20:58:37 +0700

    fix cloudinary again 2

--- backend/config/cloudinary.php
+++ backend/config/cloudinary.php
@@ -13,9 +13,18 @@ return [
 
     'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
 
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key' => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
+
+    'url' => [
+        'secure' => env('CLOUDINARY_SECURE_URL', true),
+    ],
+
     'cloud_url' => env('CLOUDINARY_URL'),
 
     'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
 
-    'secure_url' => env('CLOUDINARY_SECURE_URL', true),
 ];
