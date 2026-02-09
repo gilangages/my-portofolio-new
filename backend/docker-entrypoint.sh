9d8068cb8494c84266ed41ae33f1fda9893dfeee
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-02-09 14:54:56 +0700

    change docker-entrypoint.sh

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -8,8 +8,14 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-echo "Wiping and Reseeding Database..."
-php artisan migrate:fresh --seed --force
+# --- PERUBAHAN PENTING ---
+# Ganti 'migrate:fresh' (hapus data) menjadi 'migrate' (tambah data baru saja)
+echo "Running Migrations..."
+php artisan migrate --force
+
+# Jalankan Seeder (Aman, karena di DatabaseSeeder kita sudah pakai 'if exists' untuk admin)
+echo "Seeding Admin..."
+php artisan db:seed --force
 
 # Jalankan command utama (Apache)
 echo "Starting Apache..."
