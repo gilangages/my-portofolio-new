132f7b5c8329e7a97436eac8f6ce15b86a4b0556
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-02-09 14:51:24 +0700

    change db seed

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,29 +3,27 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // Mengecek apakah user admin sudah ada, jika belum buat baru
+        // Cek admin, kalau belum ada langsung buat pakai User::create (bukan factory)
         if (!User::where('email', 'qbdian@gmail.com')->exists()) {
-            User::factory()->create([
+
+            User::create([
                 'name' => 'Abdian',
                 'email' => 'qbdian@gmail.com',
-                // Password ini akan di-hash. Ganti 'password' dengan password yang kamu mau.
                 'password' => Hash::make('haloqbdian2121'),
+                'email_verified_at' => now(), // Opsional: biar statusnya langsung verified
             ]);
 
-            echo "Admin user created: qbdian@gmail.com / haloqbdian2121\n";
+            echo "Admin user created: qbdian@gmail.com\n";
         }
     }
 }
