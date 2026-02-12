3385833a4852dd9e9a52e195fab81b030599bdc5
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-02-12 10:56:28 +0700

    fix (ui) : setup fetch for tech section

--- frontend/src/components/Homepage/Homepage.vue
+++ frontend/src/components/Homepage/Homepage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Hero from "./Section/Hero.vue";
 import Navbar from "./Section/Navbar.vue";
+import Tech from "./Section/Tech.vue";
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import Navbar from "./Section/Navbar.vue";
     class="min-h-screen bg-white text-black font-sans selection:bg-black selection:text-white overflow-x-hidden flex flex-col pb-20 md:pb-0">
     <Navbar />
     <Hero />
+    <Tech />
   </div>
 </template>
 
--- frontend/src/components/Homepage/Section/Tech.vue
+++ frontend/src/components/Homepage/Section/Tech.vue
@@ -1,5 +1,32 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { alertError } from "../../lib/alert";
+import { getSkills } from "../../lib/api/SkillApi";
+
+const skills = ref(null);
+
+async function fetchSkills() {
+  const response = await getSkills();
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  if (response.status === 200) {
+    skills.value = responseBody;
+  } else {
+    await alertError(responseBody.message);
+  }
+}
+
+onMounted(async () => {
+  await fetchSkills();
+});
+</script>
 <template>
-  <div>
+  <div v-if="skills">
     <h1>techstack</h1>
+    <div class="text-blue-900">
+      {{ skills.name }}
+      {{ skills.category }}
+    </div>
   </div>
 </template>
