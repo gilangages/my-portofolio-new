b03b9bbc886bcd20b8c8df0e685fcc5c68953113
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-05-20 20:39:09 +0700

    feat: delete tag p (#25)

--- frontend/src/components/Me.vue
+++ frontend/src/components/Me.vue
@@ -8,7 +8,6 @@ const count = ref(0);
 <main class="bg-[#121212] h-[100vh]">
   <div id="main" class="flex flex-col items-center justify-center pt-20 text-[#f3f4f6]">
     <h1>hi, i'm Abdian. i'm a web enthusiast</h1>
-    <p></p>
     <div class="bg-blue pt-8">
       <RouterLink to="/me" class="bg-[#1e1e1e] px-8 rounded-md hover:border-white/20 border border-transparent hover:border-white/20 text-[#f3f4f6] p-2">next</RouterLink>
     </div>
